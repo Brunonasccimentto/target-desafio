a7e85427ea7f4a0e5bc42dc09f3574b632c4cb93
Bruno <brunonasccimentto@gmail.com>
2024-03-06 16:09:10 -0300

    feat: adicionando todas as respostas ao repositório.

--- teste.js
+++ teste.js
@@ -1,3 +1,6 @@
+// questão - 1
+// Resposta: 91
+
 // questão 2 - Verificar se um número pertence a sequencia de fibonacci ou não.
 
 function fibonacci(n) {
@@ -17,9 +20,24 @@ function fibonacci(n) {
   
 console.log(fibonacci(55));
 
+//questão - 3
+// Respostas:
+// a) 9
+// b) 128
+// c) 49
+// d) 100
+// e) 13
+// f) 20
+
+// 4) Primeiro ligaria duas lâmpadas e iria a primeira sala, caso estivesse apagada, já saberia a qual sala um dos interruptores pertence, e bastaria apagados os dois, ligar o terceiro e ir para uma outra sala. Caso estivesse acesa a luz da sala, desligaria um interruptor qualquer e ligaria o que faltou e iria para a próxima sala, caso apagada saberia que o interruptor que acabei de desligar corresponde àquela sala, o que mantive ligado corresponde primeira sala que visitei, e o que acabei de ligar corresponde a ultima sala. Caso acesa, saberia que a que mantive ligada da minha primeira ida corresponde a essa sala e a que apaguei seria a primeira sala, e a sala a qual não visitei estaria com a luz ligada, pois corresponde ao ultimo interruptor que liguei.
+
+
 //questão 5 - reverter uma String sem utilizar métodos prontos de strings
 
 function reverseString(string) {
     return string === '' ? '' : reverseString(string.substring(1)) + string.charAt(0);
-  }
+}
+
+console.log(reverseString("Fibonacci"));
+
   
